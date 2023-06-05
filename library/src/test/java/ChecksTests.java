5d9a22f00b322b95d0c9e319718ddc2c699f54b0
Jan Janda <janjanda42@gmail.com>
2023-06-05 10:41:52 +0200

    Test runs

---
+++ library/src/test/java/ChecksTests.java
@@ -0,0 +1,9 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+public class ChecksTests {
+    @Test
+    void addition() {
+        assertEquals(2, 2);
+    }
+}
