c24b171ad3780fe85010613890222127c01c3d56
Jan Janda <janjanda42@gmail.com>
2023-07-01 19:29:03 +0200

    Start column title check

--- library/src/main/java/otava/library/checks/ColumnTitlesCheck.java
+++ library/src/main/java/otava/library/checks/ColumnTitlesCheck.java
@@ -1,9 +1,11 @@
 package otava.library.checks;
 
+import static otava.library.utils.UrlUtils.*;
 import com.fasterxml.jackson.databind.JsonNode;
 import otava.library.exceptions.CheckCreationException;
 import otava.library.*;
 import otava.library.documents.*;
+import java.net.MalformedURLException;
 
 public final class ColumnTitlesCheck extends Check {
     public ColumnTitlesCheck(CheckFactory f) throws CheckCreationException {
@@ -17,11 +19,20 @@ public final class ColumnTitlesCheck extends Check {
 
         return null;
     }
-
-    private JsonNode findTableDescription(Table table) {
+// TODO: vyclenit extrakci tabulek z daneho deskriptoru?
+    private JsonNode findTableDescription(Table table) throws MalformedURLException {
         String tableName = table.getPreferredName();
         for (Descriptor desc : descriptors) {
-            if (desc.path("url").asText().equals(tableName)) return desc.getRootNode();
+            String baseUrl = getBaseUrl(desc);
+            JsonNode urlNode = desc.path("url");
+            if (urlNode.isTextual() && resolveUrl(baseUrl, urlNode.asText()).equals(tableName)) return desc.getRootNode();
+            JsonNode tablesArray = desc.path("tables");
+            if (urlNode.isMissingNode() && tablesArray.isArray()) {
+                for (int i = 0; i < tablesArray.size(); i++) {
+                    JsonNode arrayUrlNode = tablesArray.path(i).path("url");
+                    if (arrayUrlNode.isTextual() && resolveUrl(baseUrl, arrayUrlNode.asText()).equals(tableName)) return tablesArray.path(i);
+                }
+            }
         }
         return null;
     }
