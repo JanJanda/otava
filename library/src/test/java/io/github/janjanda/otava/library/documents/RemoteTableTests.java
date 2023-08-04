a07728bcd269c1cdd465c7cd5367c46735f77a0f
Jan Janda <janjanda42@gmail.com>
2023-08-04 10:07:51 +0200

    Add tests

--- library/src/test/java/io/github/janjanda/otava/library/documents/RemoteTableTests.java
+++ library/src/test/java/io/github/janjanda/otava/library/documents/RemoteTableTests.java
@@ -2,11 +2,12 @@ package io.github.janjanda.otava.library.documents;
 
 import static org.junit.jupiter.api.Assertions.*;
 import io.github.janjanda.otava.library.DocumentFactory;
+import io.github.janjanda.otava.library.exceptions.*;
 import org.apache.commons.csv.CSVRecord;
 import org.junit.jupiter.api.Test;
 
 class RemoteTableTests {
-    private RemoteTable createRemoteTable(String path) throws Exception {
+    private RemoteTable createRemoteTable(String path) {
         DocumentFactory df = new DocumentFactory();
         return df.makeLocalRemoteTable("src/test/resources/tables/" + path, null);
     }
@@ -25,12 +26,36 @@ class RemoteTableTests {
         assertEquals("Surname", fl1.get(0));
         assertEquals("Surname", fl2.get(0));
     }
-/*
-    @Test
-    void iterateMissingTable() throws Exception {
-        RemoteTable table = createRemoteTable("noTable");
-        for (CSVRecord row : table) {
 
+    @Test
+    void firstLineMissingTable() {
+        RemoteTable table = createRemoteTable("noTable");
+        assertThrowsExactly(ValidatorFileException.class, table::getFirstLine);
+    }
+
+    @Test
+    void iterateMissingTable() {
+        RemoteTable table = createRemoteTable("noTable");
+        assertThrowsExactly(FileIteratorException.class, () -> {for (CSVRecord row : table){}});
+    }
+
+    @Test
+    void iterateEmptyTable() {
+        RemoteTable table = createRemoteTable("table015.csv");
+        int i = 0;
+        for (CSVRecord row : table) {
+            i++;
         }
-    }*/
+        assertEquals(0, i);
+    }
+
+    @Test
+    void iterateTable() {
+        RemoteTable table = createRemoteTable("table001.csv");
+        int i = 0;
+        for (CSVRecord row : table) {
+            i++;
+        }
+        assertEquals(9, i);
+    }
 }
