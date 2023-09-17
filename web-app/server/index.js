1c81ee2e38e92a9d524f028fef0f85a36360662b
Jan Janda <janjanda42@gmail.com>
2023-09-17 16:19:52 +0200

    Adjust web app

--- web-app/server/index.js
+++ web-app/server/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+app.use('/validator', express.static(path.join(__dirname, '..', 'client', 'build')))
 
-app.listen(9000);
+app.listen(80);
