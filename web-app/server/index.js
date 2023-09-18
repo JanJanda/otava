39f173d64add70e080d9442001909cd24867d9b5
Jan Janda <janjanda42@gmail.com>
2023-09-18 09:38:55 +0200

    Organize web client

--- web-app/server/index.js
+++ web-app/server/index.js
@@ -3,6 +3,6 @@ const path = require('path');
 
 const app = express();
 
-app.use('/validator/validate', express.static(path.join(__dirname, '..', 'client', 'build')));
+app.use('/validator/validate', express.static(path.join(__dirname, '..', 'client', 'validate', 'build')));
 
 app.listen(80, () => console.log('Listening on port 80'));
