48dbea67db7f18fdead21da7e3946ffe3e36008a
Jan Janda <janjanda42@gmail.com>
2023-09-18 11:35:18 +0200

    Start web form

--- web-app/client/validate/src/App.js
+++ web-app/client/validate/src/App.js
@@ -1,9 +1,34 @@
-function App() {
+import React from "react";
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+
+export default function App() {
   return (
-    <>
-      Test
-    </>
+      <ValidationForm />
   );
 }
 
-export default App;
+function ValidationForm() {
+  return (
+    <Form method="post" action="submit-validation">
+
+      <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Label>Email address</Form.Label>
+        <Form.Control type="email" name="mailik" placeholder="Enter email" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Label>Password</Form.Label>
+        <Form.Control type="password" placeholder="Password" />
+      </Form.Group>
+
+      <Form.Group className="mb-3" controlId="formBasicCheckbox">
+        <Form.Check type="checkbox" label="Check me out" />
+      </Form.Group>
+
+      <Button variant="primary" type="submit">
+        Submit
+      </Button>
+    </Form>
+  );
+}
--- web-app/server/index.js
+++ web-app/server/index.js
@@ -4,5 +4,11 @@ const path = require('path');
 const app = express();
 
 app.use('/validator/validate', express.static(path.join(__dirname, '..', 'client', 'validate', 'build')));
+app.use(express.urlencoded({ extended: false }))
+
+app.post('/validator/validate/submit-validation', (req, res) => {
+  console.log(req.body.mailik);
+  res.send(req.body.mailik);
+});
 
 app.listen(80, () => console.log('Listening on port 80'));
