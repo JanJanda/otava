789c28df893ed2f24cf59d79d00a728f993a10e2
Jan Janda <janjanda42@gmail.com>
2023-09-20 10:35:55 +0200

    Add insert validation request

--- web-app/server/index.js
+++ web-app/server/index.js
@@ -1,17 +1,18 @@
-const express = require('express');
-const path = require('path');
-const {addValidationRequest} = require('./model');
+const express = require("express");
+const path = require("path");
+const {addValidationRequest} = require("./model");
 
 const app = express();
 
 const netPort = 80;
-const validationFormUrl = '/validator/validate';
+const validationFormUrl = "/validator/validate";
 
-app.use(validationFormUrl, express.static(path.join(__dirname, '..', 'client', 'validate', 'build')));
+app.use(validationFormUrl, express.static(path.join(__dirname, "..", "client", "validate", "build")));
 app.use(express.urlencoded({extended: true}));
 
-app.post(validationFormUrl + '/submit-validation', (req, res) => {
-  res.redirect(validationFormUrl);
+app.post(validationFormUrl + "/submit-validation", async (req, res) => {
+  const id = await addValidationRequest(req.body.language, req.body.style, req.body.passiveTables, req.body.activeTables, req.body.passiveDescriptors, req.body.activeDescriptors, req.body.description);
+  res.send(id.toString());
 });
 
-app.listen(netPort, () => console.log('Listening on port ' + netPort));
+app.listen(netPort, () => console.log("Listening on port " + netPort));
--- web-app/server/model.js
+++ web-app/server/model.js
@@ -1,4 +1,4 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 let connectionPool;
 
@@ -6,30 +6,22 @@ init();
 
 async function init() {
   connectionPool = await mysql.createPool({
-    host: 'localhost',
-    user: 'root',
+    host: "localhost",
+    user: "root",
     password: process.env.OTAVA_DB_PSW,
   });
 
-  await connectionPool.query('CREATE DATABASE IF NOT EXISTS otava;');
-  await connectionPool.query('USE otava;')
-  await connectionPool.query('CREATE TABLE IF NOT EXISTS `validations` (`id` VARCHAR(10) NOT NULL , `language` VARCHAR(10) NOT NULL , `style` VARCHAR(10) NOT NULL , `passive-tables` TEXT NOT NULL , `active-tables` TEXT NOT NULL , `passive-descriptors` TEXT NOT NULL , `active-descriptors` TEXT NOT NULL , `description` TEXT NOT NULL , `requested-time` DATETIME NOT NULL , `finished-time` DATETIME NULL , `state` VARCHAR(10) NOT NULL , `result-text` TEXT NULL , `result-json` TEXT NULL , `result-turtle` TEXT NULL , PRIMARY KEY (`id`));');
+  await connectionPool.query("CREATE DATABASE IF NOT EXISTS otava;");
+  await connectionPool.query("USE otava;")
+  await connectionPool.query("CREATE TABLE IF NOT EXISTS `validations` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT , `language` VARCHAR(10) NOT NULL , `style` VARCHAR(10) NOT NULL , `passive-tables` TEXT NOT NULL , `active-tables` TEXT NOT NULL , `passive-descriptors` TEXT NOT NULL , `active-descriptors` TEXT NOT NULL , `description` TEXT NOT NULL , `request-time` DATETIME NOT NULL , `finish-time` DATETIME NULL , `state` VARCHAR(10) NOT NULL , `result-text` TEXT NULL , `result-json` TEXT NULL , `result-turtle` TEXT NULL , PRIMARY KEY (`id`));");
 
-  console.log('Database is ready');
+  console.log("Database is ready");
 }
 
 async function addValidationRequest(language, style, passiveTables, activeTables, passiveDescriptors, activeDescriptors, description) {
-  let id = makeId(); // add transactions
-  const [rows] = await connectionPool.execute('SELECT `id` FROM `validations` WHERE `id` = ?', [id]);
-}
-
-function makeId() {
-  const chars = 'abcdefghijklmnopqrstuvwxyz0123456789';
-  let result = '';
-  for (let i = 1; i <= 10; i++) {
-    result = result + chars[Math.floor(Math.random() * chars.length)];
-  }
-  return result;
+  const result = await connectionPool.execute("INSERT INTO `validations` (`language`, `style`, `passive-tables`, `active-tables`, `passive-descriptors`, `active-descriptors`, `description`, `request-time`, `state`) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?);",
+    [language, style, passiveTables, activeTables, passiveDescriptors, activeDescriptors, description, new Date(), "waiting"]);
+  return result[0].insertId;
 }
 
 module.exports.addValidationRequest = addValidationRequest;
