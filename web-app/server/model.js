d0dbc7a02e1fa7b177e2d399a5f57852ef1b8b1b
Jan Janda <janjanda42@gmail.com>
2023-09-21 08:27:57 +0200

    Extend web model

--- web-app/server/model.js
+++ web-app/server/model.js
@@ -15,7 +15,15 @@ async function init() {
   await connectionPool.query("USE otava;")
   await connectionPool.query("CREATE TABLE IF NOT EXISTS `validations` (`id` INT UNSIGNED NOT NULL AUTO_INCREMENT , `language` VARCHAR(10) NOT NULL , `style` VARCHAR(10) NOT NULL , `passive-tables` TEXT NOT NULL , `active-tables` TEXT NOT NULL , `passive-descriptors` TEXT NOT NULL , `active-descriptors` TEXT NOT NULL , `description` TEXT NOT NULL , `request-time` DATETIME NOT NULL , `finish-time` DATETIME NULL , `state` VARCHAR(10) NOT NULL , `result-text` TEXT NULL , `result-json` TEXT NULL , `result-turtle` TEXT NULL , PRIMARY KEY (`id`));");
 
+  setInterval(deleteOld, 3600000);
+
   console.log("Database is ready");
+}
+
+async function deleteOld() {
+  const d = new Date();
+  d.setDate(d.getDate() - 7);
+  await connectionPool.execute("DELETE FROM `validations` WHERE `request-time` < ?", [d]);
 }
 
 async function addValidationRequest(language, style, passiveTables, activeTables, passiveDescriptors, activeDescriptors, description) {
